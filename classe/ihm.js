74ab69da0284338108ef1f87fb4aaa8cf73ad435
Leo Lhomme <leolhommeo@gmail.com>
2023-03-07 16:04:27 +0100

    ajout de la victoire

--- classe/ihm.js
+++ classe/ihm.js
@@ -4,32 +4,33 @@ export class Ihm{
   constructor(){
     this.gametable=[[" "," "," "],[" "," "," "],[" "," "," "]]
     this.victory=false
-  }
+    this.compteur = 0
+    }
 
   async menu(){
     let choixX
     let choixY
-    let compteur=0
     this.affichageTableau()
     do{
-      console.log("tour de :",compteur%2 ?"O" :"X")
+      console.log("tour de :",this.compteur%2 ?"O" :"X")
       choixX = await poserUneQuestion("\n\nentrer la valeur de la ligne :")
       if(+choixX === -1){
-        compteur=15
+        this.compteur=15
       }
       choixY = await poserUneQuestion("\n\nentrer la valeur de la colone :")
 
       if(choixX<4 && choixX>0 && choixY<4 && choixY>0){
         if(this.gametable[Number(choixX)-1][Number(choixY)-1] === " "){
-          this.gametable[Number(choixX)-1][Number(choixY)-1] = compteur%2 ?"O" :"X"
+          this.gametable[Number(choixX)-1][Number(choixY)-1] = this.compteur%2 ?"O" :"X"
           this.affichageTableau()
-          compteur++
+          this.compteur++
           this.testWin(Number(choixX)-1,Number(choixY)-1)
         }
       }
 
-    }while(compteur<10 && this.victory!=true)
+    }while(this.compteur<10 && this.victory!=true)
     this.affichageTableau()
+    console.log("victoire de : ",this.compteur%2 ===0 ? "O" :"X")
     }
 
     affichageTableau(){
@@ -47,24 +48,23 @@ export class Ihm{
     }
 
     testEnLigne(ligne){
-      console.log(this.gametable[ligne][0] === this.gametable[ligne][1] === this.gametable[ligne][2])
-      if(this.gametable[ligne][0] === this.gametable[ligne][1] === this.gametable[ligne][2] && this.gametable[ligne][0]!== " "){
+      if((this.gametable[ligne][0] === this.gametable[ligne][1]) && (this.gametable[ligne][0] === this.gametable[ligne][2]) && this.gametable[ligne][0]!== " "){
         console.log("testligne")
         this.victory=true
       }
     }
     testColone(colone){
-      if(this.gametable[0][colone] === this.gametable[1][colone] === this.gametable[2][colone] && this.gametable[0][colone] !==" "){
+      if((this.gametable[0][colone] === this.gametable[1][colone]) && (this.gametable[0][colone]=== this.gametable[2][colone]) && this.gametable[0][colone] !==" "){
         console.log("testcolone")
         this.victory=true
       }
     }
     testDiago(){
-      if(this.gametable[0][0] === this.gametable[1][1] === this.gametable[2][2] && this.gametable[0][0] !==" "){
+      if((this.gametable[0][0] === this.gametable[1][1]) && (his.gametable[0][0]=== this.gametable[2][2]) && this.gametable[0][0] !==" "){
         console.log("testdiago")
         this.victory=true
       }
-      if(this.gametable[0][2] === this.gametable[1][1] === this.gametable[2][0] && this.gametable[1][1] !==" "){
+      if((this.gametable[0][2] === this.gametable[1][1]) && (this.gametable[0][2]=== this.gametable[2][0]) && this.gametable[1][1] !==" "){
         console.log("testdiago2")
         this.victory=true
       }
